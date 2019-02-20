e7aa4f7e8737123017d8a83b0ec988199351c699
jcchoate <jaredchoatepro@gmail.com>
2019-02-19 19:35:07 -0700

    first commit

---
+++ src/trainers/Testtrainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Testtrainer extends Component {
+  render() {
+    return (
+      <div className="trainer">
+        <img src="https://archives.bulbagarden.net/media/upload/1/10/Maximo_OD.png"/>
+      </div>
+    )
+  }
+}
